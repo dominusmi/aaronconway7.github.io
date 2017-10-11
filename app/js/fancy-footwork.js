051e311225ea2d8ad48009ea897335c4d54b88ce
aaronconway7 <aaronconway796@gmail.com>
2017-10-11 23:06:10 +0100

    ting goes skrrrrr

--- app/js/fancy-footwork.js
+++ app/js/fancy-footwork.js
@@ -11,7 +11,7 @@ $(window).on("load", function() {
 function typed(){
     Typed.new('.typed', {
         // strings: ["Hello World!^500", "Hi.", "Yo.", "Sup.", "Wag1.", "What'gwanin?", "Hello.", "What's up?"],
-        strings: ["The ting goes...", "skrrrahh!", "Pap, pap, ka-ka-ka", "Skidiki-pap-pap!", "And a pu-pu-drrrr-boom!", "Skya!", "Du-du-ku-ku-pun-pun!", "Poom, poom", "You dun know, Big Aaron"],
+        strings: ["The ting goes...", "skrrrahh!", "Pap, pap, ka-ka-ka", "Skidiki-pap-pap!", "And a pu-pu-drrrr-boom!", "Skya!", "Du-du-ku-ku-pun-pun!", "Poom, poom", "You dun know, Big Aaron^500"],
         // typeSpeed: 10,
         typeSpeed: 30,
         loop: true,
