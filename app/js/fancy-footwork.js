51e2aad6e21c7b496d9a3ee6c05dfce44c8a08b9
aaronconway7 <aaronconway796@gmail.com>
2018-01-27 23:26:42 +0000

    big changes

--- app/js/fancy-footwork.js
+++ app/js/fancy-footwork.js
@@ -1,36 +1,18 @@
 $(document).ready(function(){
     console.log("I see you peeking 👀");
     window.sr = ScrollReveal();
-});
-
-$(window).on("load", function() {
-    var page = location.hash.slice(1).replace(/#/g , '/');
-    if (page == "") {
-        page = "home";
-    }
-    var href = page +".html";
-    $('main').attr('id', page);
-    $('main').load(href, function(){
-        setTimeout(function(){
-            $('#index').addClass('loaded');
-            if (page == "home") {
-                typed();
-            } else if (page == "portfolio") {
-                sr.reveal('.work');
-            }
-        }, 5000);
+    sr.reveal('.reveal');
+    $('.js-tilt').tilt({
+        perspective: 2000,
+        scale: 1.2
     });
 });
 
-$('main').on("load", function(){
-    // var page = location.hash.slice(1).replace(/#/g , '/');
-    // setTimeout(function(){
-    //     $('#index').addClass('loaded');
-    //     if (page == "home") {
-    //         typed();
-    //     }
-    // }, onLoad()+5000);
-    // window.sr = new scrollReveal();
+$(window).on("load", function() {
+    setTimeout(function(){
+        $('#index').addClass('loaded');
+        typed();
+    }, onLoad() + 500);
 });
 
 function typed(){
@@ -48,47 +30,81 @@ function onLoad() {
 }
 
 $(window).on('hashchange', function() {
-    $('body').removeClass('loaded');
     var page = location.hash.slice(1).replace(/#/g , '/');
-    if (page == "") {
-        page = "home";
-    }
-    var href = page +".html";
-    setTimeout(function(){
-        $('main').attr('id', page);
-        $('main').load(href, function(){
-            setTimeout(function(){
-                $('#index').addClass('loaded');
-                if (page == "home") {
-                    typed();
-                } else if (page == "portfolio") {
-                    sr.reveal('.work');
-                }
-            }, 0);
+    if (page.indexOf("/") >= 0) {
+        var href = page +".html";
+        $('#portfolio-pull-out').load(href, function(){
+            $(this).show();
+            console.log("loaded");
         });
-    }, 1500)
+    }
 });
 
-$('body').on("click", '#load-more', function(){
-    $('.work.old').toggle();
-    $(this).find('h1').html("Hide old work!");
+// $('body').on("click", '#load-more', function(){
+//     $('.work.old').toggle();
+//     $(this).find('h1').html("Hide old work!");
+// });
+//
+// $('body').on("click", '.theme-change', function(){
+//     $(this).toggleClass('theme-black');
+//     $('#portfolio').toggleClass('black')
+// });
+//
+// $('body').on("click", '.small-grid', function(){
+//     $('#grid').addClass('small');
+// });
+//
+// $('body').on("click", '.big-grid', function(){
+//     $('#grid').removeClass('small');
+// });
+//
+// $('body').on("mouseenter", '.work', function(){
+//     $(this).siblings('.work').addClass('hovered');
+// }).on("mouseleave", '.work', function(){
+//     $(this).siblings('.work').removeClass('hovered');
+// });
+
+$('.show-old-work').click(function(){
+    $('#portfolio .item.old').toggle();
+    if ($(this).find('h3').text() == "Show my old work!") {
+        $(this).find('h3').text("Hide my old work!");
+    } else {
+        $(this).find('h3').text("Show my old work!");
+    }
 });
 
-$('body').on("click", '.theme-change', function(){
-    $(this).toggleClass('theme-black');
-    $('#portfolio').toggleClass('black')
+var portfolioScrolled = false;
+
+$(window).scroll(function(e){
+    var wScroll = $(this).scrollTop();
+
+    if (wScroll >= $('#portfolio').offset().top) {
+        if (!portfolioScrolled) {
+            $(this).scrollTop($('#portfolio').offset().top);
+            $('html').addClass('lock');
+        }
+    } else {
+        if ($('.portfolio-panel').scrollTop() + $('.portfolio-panel').innerHeight() >= $('.portfolio-panel')[0].scrollHeight) {
+            portfolioScrolled = true;
+        } else {
+            portfolioScrolled = false;
+        }
+    }
+
 });
 
-$('body').on("click", '.small-grid', function(){
-    $('#grid').addClass('small');
+$('.portfolio-panel').scroll(function(){
+    var wScroll = $(this).scrollTop();
+    var iHeight = $(this).innerHeight();
+
+    if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+        portfolioScrolled = true;
+        $('html').removeClass('lock');
+    }
+
 });
 
-$('body').on("click", '.big-grid', function(){
-    $('#grid').removeClass('small');
-});
-
-$('body').on("mouseenter", '.work', function(){
-    $(this).siblings('.work').addClass('hovered');
-}).on("mouseleave", '.work', function(){
-    $(this).siblings('.work').removeClass('hovered');
+$('body').on("click", '#portfolio-pull-out .close-panel', function(){
+    $('#portfolio-pull-out').hide();
+    window.location.hash="portfolio";
 });
